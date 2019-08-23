43c00decbeb3d93a07cb29553021c116bc4c3237
Max <max.zhou@nike.com>
2019-08-23 11:03:27 +0800

    add mutil cache;

--- showcase-common/src/main/java/org/max/learning/common/controller/CacheController.java
+++ showcase-common/src/main/java/org/max/learning/common/controller/CacheController.java
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.max.learning.common.dto.Entity;
 import org.max.learning.common.dto.User;
-import org.max.learning.common.service.RedisTemplateServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
@@ -20,6 +19,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin
@@ -27,24 +29,31 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/cache")
 public class CacheController {
 	
+	private Cache<String, Entity> cache = Caffeine.newBuilder()
+			.initialCapacity(99)
+			.maximumSize(999)
+            .expireAfterWrite(1, TimeUnit.MINUTES)
+            .build();
+	
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
-//	private RedisTemplateServiceImpl redisTemplateService;
 	
 	@ApiOperation(value = "get from cache", notes = "get from cache")
     @GetMapping("/get/{key}")
     public ResponseEntity<Entity> get(@PathVariable(name = "key") String key) {
-//		Entity e = redisTemplateService.get(key, Entity.class);
-		Entity e = (Entity)redisTemplate.opsForValue().get(key);
-		return new ResponseEntity<Entity>(e, HttpStatus.OK);
+//		Entity e = (Entity)redisTemplate.opsForValue().get(key);
+		Entity entity = cache.get(key, k -> getFromRedis(k));
+		return new ResponseEntity<Entity>(entity, HttpStatus.OK);
     }
 	
     @ApiOperation(value = "post into cache", notes = "post into cache")
-    @GetMapping("/post")
-    public ResponseEntity<String> post() {
+    @GetMapping("/post/{key}/{value}")
+    public ResponseEntity<String> post(
+    		@PathVariable(name = "key") String key,
+    		@PathVariable(name = "value") String value) {
     	Entity e = new Entity();
-    	e.setKey("key:" + Instant.now().getEpochSecond());
-    	e.setValue("value");
+    	e.setKey(key);
+    	e.setValue(value);
     	List<User> users = new ArrayList<User>();
     	for(long i=0;i<6;i++) {
     		User u = new User();
@@ -55,10 +64,12 @@ public class CacheController {
     		users.add(u);
     	}
     	e.setUserList(users);
-    	
-//    	redisTemplateService.set(e.getKey(), e);
-    	redisTemplate.opsForValue().set(e.getKey(), e, 3, TimeUnit.MINUTES);
+    	redisTemplate.opsForValue().set(e.getKey(), e, 5, TimeUnit.MINUTES);
         return new ResponseEntity<String>(e.getKey(), HttpStatus.OK);
+    }
+    
+    private Entity getFromRedis(String key) {
+    	return (Entity)redisTemplate.opsForValue().get(key);
     }
     
 }
