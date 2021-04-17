7c07e98fd391a08bd6353862fab744dd37032f85
zhoujun <zhoujun9633@163.com>
2021-04-18 00:56:45 +0800

    Update UserController.java

--- exam/src/main/java/org/max/exam/controller/UserController.java
+++ exam/src/main/java/org/max/exam/controller/UserController.java
@@ -7,8 +7,10 @@ import org.max.exam.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +33,15 @@ public class UserController {
         return new ResponseEntity<>("deleted", HttpStatus.OK);
     }
     
-    @PostMapping(value = "/find")
+    @GetMapping(value = "/find")
     public ResponseEntity<List<User>> findAll() {
     	List<User> userList = userService.findAll();
         return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+    
+    @PutMapping(value = "/login")
+    public ResponseEntity<Boolean> login(@RequestBody User user) {
+        return new ResponseEntity<>(userService.check(user.getUsername(), user.getPassword()), HttpStatus.OK);
     }
     
 }
